bf595ccd079eb671be7538ce1d4bf1ea9f39ab94
SamriddhVerma <132701035+SamriddhVerma@users.noreply.github.com>
2023-09-26 04:00:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,4 +2,5 @@ function contactus()
 {
     document.getElementById('bodyi').classList.toggle('blur')
     document.getElementById('contact').classList.toggle("hidden")
+    document.getElementById('idk').classList.toggle("hidden")
 }
